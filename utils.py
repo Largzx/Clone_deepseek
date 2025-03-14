0e5fd163771c4699fb5cee38aa016bf950c391dc
Largzx <2817407674@qq.com>
2025-03-14 17:08:44 +0800

    简洁后端

--- utils.py
+++ utils.py
@@ -14,8 +14,3 @@ def get_chat_response(prompt, memory, model, openai_api_key, temperature):
     response = chain.invoke({"input": prompt})
     return response["response"]
 
-
-if __name__ == '__main__':
-    memory = ConversationBufferMemory(return_messages=True)
-    print(get_chat_response("今天是几号", memory, "deepseek-chat", os.environ["OPENAI_API_KEY"]))
-    # print(get_chat_response("", memory, "deepseek-chat", os.environ["OPENAI_API_KEY"]))
